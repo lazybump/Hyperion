7f461ff29597d7674d8ac29a451387756ddd4865
lazybump <104283344+lazybump@users.noreply.github.com>
2023-01-30 18:06:02 +0000

    add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
 function App() {
-  return <div>hello world</div>;
+  return (
+    <div className="bg-slate-100 h-screen">
+      <header className="bg-white p-4">
+        <div className="flex items-center justify-around">
+          <img src="/assets/images/url.png" alt="/" className="h-4" />
+          <p>Get mentor support</p>
+          <div className="flex space-x-2">
+            <img
+              src="/assets/images/student.png"
+              alt="profile"
+              className="h-8 rounded-full"
+            />
+            <span>Profile</span>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
