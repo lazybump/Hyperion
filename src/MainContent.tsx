bef7664f4cb8d1a141437f27dc5a0153ba8b03ad
lazybump <104283344+lazybump@users.noreply.github.com>
2023-01-30 22:14:53 +0000

    build bottom section

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -1,17 +1,43 @@
+import { useState } from "react";
+
+interface TopicsType {
+  id: number;
+  name: string;
+}
+
+const topics: TopicsType[] = [
+  { id: 1, name: "Industry Knowledge 1" },
+  { id: 2, name: "Industry Knowledge 2" },
+  { id: 3, name: "HTML Basics" },
+  { id: 4, name: "Your First PHP Script" },
+];
+
 const MainContent = () => {
   return (
-    <main className="border-2 border-blue-700 bg-white m-4 p-8">
-      <section className="border-b grid grid-cols-2 grid-rows-2 items-center">
+    <main className="bg-white m-4 p-8 rounded-md">
+      <section className="grid grid-cols-2 grid-rows-2 items-center">
         <img
           src="/assets/images/beginner-php-fundamentals.png"
-          className="h-14"
+          className="h-12"
         />
         <h1 className="text-sm">Web Development Bootcamp Trial</h1>
         <button className="col-start-2 bg-primary text-white text-sm rounded-lg py-2">
           Continue your trial
         </button>
       </section>
-      <section>bottom</section>
+      <section className="border-t mt-8 pt-8">
+        <h2 className="text-center mb-6">Your progress (1/4) complete</h2>
+        <div className="flex flex-col space-y-3">
+          {topics.map((topic) => (
+            <button
+              key={topic.id}
+              className="border-[1px] border-primary rounded-lg py-2 text-primary"
+            >
+              {topic.name}
+            </button>
+          ))}
+        </div>
+      </section>
     </main>
   );
 };
