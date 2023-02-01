48879a76be9bc129650db74f37e87a885787da17
lazybump <104283344+lazybump@users.noreply.github.com>
2023-02-01 22:32:04 +0000

    add progress count. adding conditional styling to topics WIP

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -1,18 +1,46 @@
 import { useState } from "react";
+import TopicCard from "./TopicCard";
 
 interface TopicsType {
   id: number;
   name: string;
+  isClicked: boolean;
 }
 
 const topics: TopicsType[] = [
-  { id: 1, name: "Industry Knowledge 1" },
-  { id: 2, name: "Industry Knowledge 2" },
-  { id: 3, name: "HTML Basics" },
-  { id: 4, name: "Your First PHP Script" },
+  { id: 1, name: "Industry Knowledge 1", isClicked: false },
+  { id: 2, name: "Industry Knowledge 2", isClicked: false },
+  { id: 3, name: "HTML Basics", isClicked: false },
+  { id: 4, name: "Your First PHP Script", isClicked: false },
 ];
 
+interface CompletedTopicType {
+  id: number;
+}
+
 const MainContent = () => {
+  // Array of ID objects. Each one represents a different topic button
+  const [completedTopics, setCompletedTopics] = useState<CompletedTopicType[]>(
+    []
+  );
+
+  const handleClick = (id: number) => {
+    console.log(id);
+
+    setCompletedTopics((prev) => {
+      // tries to find the topic object in the completed list
+      const retrievedTopic = prev.find((topic) => topic.id === id);
+      // if it exists in the array...
+      if (retrievedTopic) {
+        return prev.filter((topic) => topic.id !== id);
+      } else {
+        return [...prev, { id }];
+      }
+    });
+  };
+
+  const total = completedTopics.length;
+
   return (
     <main className="bg-white m-4 p-8 rounded-md">
       <section className="grid grid-cols-2 grid-rows-2 items-center">
@@ -26,15 +54,10 @@ const MainContent = () => {
         </button>
       </section>
       <section className="border-t mt-8 pt-8">
-        <h2 className="text-center mb-6">Your progress (1/4) complete</h2>
+        <h2 className="text-center mb-6">Your progress ({total}/4) complete</h2>
         <div className="flex flex-col space-y-3">
           {topics.map((topic) => (
-            <button
-              key={topic.id}
-              className="border-[1px] border-primary rounded-lg py-2 text-primary"
-            >
-              {topic.name}
-            </button>
+            <TopicCard key={topic.id} {...topic} handleClick={handleClick} />
           ))}
         </div>
       </section>
