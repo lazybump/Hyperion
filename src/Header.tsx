2b5483dae749b0c59aa53c567e90e667b8b4a1ca
lazybump <104283344+lazybump@users.noreply.github.com>
2023-02-04 22:35:23 +0000

    make header responsive. add dropdown on hover

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,14 +3,27 @@ const Header = () => {
     <header className="bg-white p-3">
       <nav className="flex items-center justify-around">
         <img src="/assets/images/url.png" alt="/" className="h-4" />
-        <p className="text-sm font-bold">Get mentor support</p>
-        <div className="flex space-x-1 items-center">
-          <img
-            src="/assets/images/student.png"
-            alt="profile"
-            className="h-5 rounded-full"
-          />
-          <span className="font-bold">Profile</span>
+        <h3 className="md:hidden text-sm font-bold">Get mentor support</h3>
+        <h3 className="hidden md:block flex-grow ml-12 text-primary font-bold">
+          Full Stack Web Developer Bootcamp
+        </h3>
+        <div className="bg-white relative w-32 group cursor-pointer pr-4 pl-2">
+          <div className="flex justify-between">
+            <img
+              src="/assets/images/student.png"
+              alt="profile"
+              className="h-5 rounded-full inline"
+            />
+            <span className="font-bold">Profile</span>
+          </div>
+          <ul className="hidden group-hover:block group-hover:bg-white absolute right-0 left-0 p-4 font-bold space-y-1">
+            <li>
+              <a href="/">Edit Profile</a>
+            </li>
+            <li>
+              <a href="/">Log out</a>
+            </li>
+          </ul>
         </div>
       </nav>
     </header>
